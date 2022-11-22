64bcce92fa4542299dcf15088d93cf79b1a218db
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-11-22 10:44:34 +0300

    Create colors.js

---
+++ src/theme/colors.js
@@ -0,0 +1,24 @@
+const LightTheme = {
+    colors: {
+        primary: '#fc6b68',
+        secondary: '#003359',
+        lightGray: '#f6f6f6',
+        darkGray: '#929397',
+        background: '#f2f1f6',
+        textPrimary: '#111',
+        textSecondary: '#A9A9A9'
+    },
+};
+const DarkTheme = {
+    colors: {
+        primary: '#fc6b68',
+        secondary: '#003359',
+        lightGray: '#171717',
+        darkGray: '#A9A9A9',
+        background: '#010101',
+        textPrimary: '#fff',
+        textSecondary: '#A9A9A9'
+    },
+};
+
+export {LightTheme, DarkTheme};
