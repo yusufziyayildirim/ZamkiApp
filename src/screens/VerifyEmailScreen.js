1c104a3db2e6dca16fd9bb43e1994197fefb179f
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-11-22 10:47:58 +0300

    VerifyEmailScreen design

--- src/screens/VerifyEmailScreen.js
+++ src/screens/VerifyEmailScreen.js
@@ -1,8 +1,37 @@
-import { Text } from "react-native";
+import { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View } from "react-native";
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { useTheme } from '@react-navigation/native';
+
+//Components
+import AuthInput from "../components/AuthInput";
+import AuthPageDesc from "../components/AuthPageDesc";
+import ActionButton from "../components/ActionButton";
+import DismissKeyboard from "../components/DismissKeyboard";
 
 const VerifyEmailScreen = ({ navigation }) => {
+  const colors = useTheme().colors;
+  const [email, setEmail] = useState("");
+
+  const forgotPasswordSubmit = () => {
+    navigation.navigate('VerifyEmail')
+  }
+
   return (
-    <Text>ForgotPasswordScreen</Text>
+    <SafeAreaView>
+      <DismissKeyboard>
+        <KeyboardAwareScrollView style={{ height: "100%" }}>
+          <View style={{ paddingTop: 50, paddingHorizontal: 7, backgroundColor: colors.gray }}>
+            <AuthPageDesc title="Almost done!" subTitle={`We will send verify \n email!`} />
+            <View style={{ paddingHorizontal: 25, marginTop: 25 }}>
+              <AuthInput placeholder="Email" value={email} onChange={setEmail} />
+              <ActionButton disabled={false} value="Send verify email" onPress={forgotPasswordSubmit} />
+            </View>
+          </View>
+        </KeyboardAwareScrollView>
+      </DismissKeyboard>
+    </SafeAreaView>
   )
 }
 
