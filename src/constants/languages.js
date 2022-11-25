d30de2eaee8fb732deab69470f387e33d87480e6
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-11-25 14:55:52 +0300

    Create languages data

---
+++ src/constants/languages.js
@@ -0,0 +1,394 @@
+export const languages = [
+    {
+        id: 1,
+        name: "العربية",
+        code: "ar",
+        isoName : "Arabic",
+        img : require('../assets/images/flags/ar.png')
+    },
+    {
+        id: 2,
+        name: "Azərbaycan Dili",
+        code: "az",
+        isoName: "Azerbaijani",
+        img : require('../assets/images/flags/az.png')
+    },
+    {
+        id: 3,
+        name: "বাংলা",
+        code: "bn",
+        isoName: "Bengali",
+        img : require('../assets/images/flags/bn.png')
+    },
+    {
+        id: 4,
+        name: "भोजपुरी",
+        code: "bho",
+        isoName: "Bhojpuri",
+        img : require('../assets/images/flags/bho.png')
+    },
+    {
+        id: 5,
+        name: "български език",
+        code: "bg",
+        isoName: "Bulgarian",
+        img : require('../assets/images/flags/bg.png')
+    },
+    {
+        id: 6,
+        name: "Burmese",
+        code: "my",
+        isoName: "Burmese",
+        img : require('../assets/images/flags/my.png')
+    },
+    {
+        id: 8,
+        name: "中文 (简体)",
+        code: "zh",
+        isoName: "Chinese (Simplified)",
+        img : require('../assets/images/flags/zh.png')
+    },
+    {
+        id: 9,
+        name: "中文 (繁體)",
+        code: "zhHant",
+        isoName: "Chinese (Traditional)",
+        img : require('../assets/images/flags/zhHant.png')
+    },
+    {
+        id: 10,
+        name: "Hrvatski",
+        code: "hr",
+        isoName: "Croatian",
+        img : require('../assets/images/flags/hr.png')
+    },
+    {
+        id: 11,
+        name: "Čeština",
+        code: "cs",
+        isoName: "Czech",
+        img : require('../assets/images/flags/cs.png')
+    },
+    {
+        id: 12,
+        name: "Dansk",
+        code: "da",
+        isoName: "Danish",
+        img : require('../assets/images/flags/da.png')
+    },
+    {
+        id: 13,
+        name: "Nederlands",
+        code: "nl",
+        isoName: "Dutch",
+        img : require('../assets/images/flags/nl.png')
+    },
+    {
+        id: 14,
+        name: "English",
+        code: "en",
+        isoName: "English",
+        img : require('../assets/images/flags/en.png')
+    },
+    {
+        id: 15,
+        name: "Eesti",
+        code: "et",
+        isoName: "Estonian",
+        img : require('../assets/images/flags/et.png')
+    },
+    {
+        id: 16,
+        name: "Suomi",
+        code: "fi",
+        isoName: "Finnish",
+        img : require('../assets/images/flags/fi.png')
+    },
+    {
+        id: 17,
+        name: "Français",
+        code: "fr",
+        isoName: "French",
+        img : require('../assets/images/flags/fr.png')
+    },
+    {
+        id: 18,
+        name: "Deutsch",
+        code: "de",
+        isoName: "German",
+        img : require('../assets/images/flags/de.png')
+    },
+    {
+        id: 19,
+        name: "ελληνικά",
+        code: "el",
+        isoName: "Greek",
+        img : require('../assets/images/flags/el.png')
+    },
+    {
+        id: 20,
+        name: "ગુજરાતી",
+        code: "gu",
+        isoName: "Gujarati",
+        img : require('../assets/images/flags/gu.png')
+    },
+    {
+        id: 21,
+        name: "هَوُسَ",
+        code: "ha",
+        isoName: "Hausa",
+        img : require('../assets/images/flags/ha.png')
+    },
+    {
+        id: 22,
+        name: "हिन्दी",
+        code: "hi",
+        isoName: "Hindi",
+        img : require('../assets/images/flags/hi.png')
+    },
+    {
+        id: 23,
+        name: "Magyar",
+        code: "hu",
+        isoName: "Hungarian",
+        img : require('../assets/images/flags/hu.png')
+    },
+    {
+        id: 24,
+        name: "Bahasa Indonesia",
+        code: "id",
+        isoName: "Indonesian",
+        img : require('../assets/images/flags/id.png')
+    },
+    {
+        id: 25,
+        name: "Gaeilge",
+        code: "ga",
+        isoName: "Irish",
+        img : require('../assets/images/flags/ga.png')
+    },
+    {
+        id: 26,
+        name: "Italiano",
+        code: "it",
+        isoName: "Italian",
+        img : require('../assets/images/flags/it.png')
+    },
+    {
+        id: 27,
+        name: "日本語",
+        code: "ja",
+        isoName: "Japanese",
+        img : require('../assets/images/flags/ja.png')
+    },
+    {
+        id: 28,
+        name: "basa Jawa",
+        code: "jv",
+        isoName: "Javanese",
+        img : require('../assets/images/flags/jv.png')
+    },
+    {
+        id: 29,
+        name: "ಕನ್ನಡ",
+        code: "kn",
+        isoName: "Kannada",
+        img : require('../assets/images/flags/kn.png')
+    },
+    {
+        id: 30,
+        name: "한국어",
+        code: "ko",
+        isoName: "Korean",
+        img : require('../assets/images/flags/ko.png')
+    },
+    {
+        id: 31,
+        name: "Latviešu",
+        code: "lv",
+        isoName: "Latvian",
+        img : require('../assets/images/flags/lv.png')
+    },
+    {
+        id: 32,
+        name: "Lietuvių",
+        code: "lt",
+        isoName: "Lithuanian",
+        img : require('../assets/images/flags/lt.png')
+    },
+    {
+        id: 33,
+        name: "македонски јазик",
+        code: "mk",
+        isoName: "Macedonian",
+        img : require('../assets/images/flags/mk.png')
+    },
+    {
+        id: 34,
+        name: "मैथिली/মৈথিলী",
+        code: "mai",
+        isoName: "Maithili",
+        img : require('../assets/images/flags/mai.png')
+    },
+    {
+        id: 35,
+        name: "മലയാളം",
+        code: "ml",
+        isoName: "Malayalam",
+        img : require('../assets/images/flags/ml.png')
+    },
+    {
+        id: 36,
+        name: "Malti",
+        code: "mt",
+        isoName: "Maltese",
+        img : require('../assets/images/flags/mt.png')
+    },
+    {
+        id: 37,
+        name: "मराठी",
+        code: "mr",
+        isoName: "Marathi",
+        img : require('../assets/images/flags/mr.png')
+    },
+    {
+        id: 38,
+        name: "Norsk",
+        code: "no",
+        isoName: "Norwegian",
+        img : require('../assets/images/flags/no.png')
+    },
+    {
+        id: 39,
+        name: "فارسی",
+        code: "fa",
+        isoName: "Persian",
+        img : require('../assets/images/flags/fa.png')
+    },
+    {
+        id: 40,
+        name: "Português",
+        code: "pt",
+        isoName: "Portuguese",
+        img : require('../assets/images/flags/pt.png')
+    },
+    {
+        id: 41,
+        name: "Română",
+        code: "ro",
+        isoName: "Romanian",
+        img : require('../assets/images/flags/ro.png')
+    },
+    {
+        id: 42,
+        name: "Русский",
+        code: "ru",
+        isoName: "Russian",
+        img : require('../assets/images/flags/ru.png')
+    },
+    {
+        id: 43,
+        name: "Српски",
+        code: "sr",
+        isoName: "Serbian",
+        img : require('../assets/images/flags/sr.png')
+    },
+    {
+        id: 44,
+        name: "सिन्धी, سنڌي، سندھی",
+        code: "sd",
+        isoName: "Sindhi",
+        img : require('../assets/images/flags/sd.png')
+    },
+    {
+        id: 45,
+        name: "Slovenský",
+        code: "sk",
+        isoName: "Slovak",
+        img : require('../assets/images/flags/sk.png')
+    },
+    {
+        id: 46,
+        name: "Slovenščina",
+        code: "sl",
+        isoName: "Slovenian",
+        img : require('../assets/images/flags/sl.png')
+    },
+    {
+        id: 47,
+        name: "Español",
+        code: "es",
+        isoName: "Spanish",
+        img : require('../assets/images/flags/es.png')
+    },
+    {
+        id: 48,
+        name: "Basa Sunda",
+        code: "su",
+        isoName: "Sundanese",
+        img : require('../assets/images/flags/su.png')
+    },
+    {
+        id: 49,
+        name: "Svenska",
+        code: "sv",
+        isoName: "Swedish",
+        img : require('../assets/images/flags/sv.png')
+    },
+    {
+        id: 50,
+        name: "தமிழ்",
+        code: "ta",
+        isoName: "Tamil",
+        img : require('../assets/images/flags/ta.png')
+    },
+    {
+        id: 51,
+        name: "తెలుగు",
+        code: "te",
+        isoName: "Telugu",
+        img : require('../assets/images/flags/te.png')
+    },
+    {
+        id: 52,
+        name: "ไทย",
+        code: "th",
+        isoName: "Thai",
+        img : require('../assets/images/flags/th.png')
+    },
+    {
+        id: 53,
+        name: "Türkçe",
+        code: "tr",
+        isoName: "Turkish",
+        img : require('../assets/images/flags/tr.png')
+    },
+    {
+        id: 54,
+        name: "Українська",
+        code: "uk",
+        isoName: "Ukrainian",
+        img : require('../assets/images/flags/uk.png')
+    },
+    {
+        id: 55,
+        name: "اردو",
+        code: "ur",
+        isoName: "Urdu",
+        img : require('../assets/images/flags/ur.png')
+    },
+    {
+        id: 56,
+        name: "Tiếng Việt",
+        code: "vi",
+        isoName: "Vietnamese",
+        img : require('../assets/images/flags/vi.png')
+    },
+    {
+        id: 57,
+        name: "Yorùbá",
+        code: "yo",
+        isoName: "Yoruba",
+        img : require('../assets/images/flags/yo.png')
+    }
+]
