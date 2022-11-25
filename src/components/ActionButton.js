ef03c274e070640f067b04458b328171941825bc
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-11-25 14:56:00 +0300

    Update ActionButton.js

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -1,16 +1,15 @@
 import { useTheme } from "@react-navigation/native";
 import { Text, TouchableOpacity } from "react-native";
 
-const ActionButton = ({ value, onPress = null, disabled = false }) => {
+const ActionButton = ({ value, onPress = null, disabled = false, height, width }) => {
     const colors = useTheme().colors;
     const handlePress = () => {
         onPress()
     }
-
     return (
         <TouchableOpacity
             disabled={disabled}
-            style={{ backgroundColor: colors.primary, height: 55, borderRadius: 15, alignItems: "center", justifyContent: "center", marginTop: 10 }}
+            style={{ backgroundColor: colors.primary, height: height ?? 55, width: width ?? "100%" , borderRadius: 15, alignItems: "center", justifyContent: "center", marginTop: 10 }}
             onPress={handlePress}
         >
             {disabled
