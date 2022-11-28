1d68fac006bd4df313e7a410af0e67c38ec0e853
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-11-28 11:01:05 +0300

    Create headerConfig.js

---
+++ src/utils/headerConfig.js
@@ -0,0 +1,20 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const getToken = async () => {
+    try {
+        const token = await AsyncStorage.getItem('userToken');
+        return token
+    } catch (e) {
+        // save error
+    }
+}
+
+export default async function headerConfig() {
+    const token = await getToken()
+
+    if (token) {
+        return { Authorization: `Bearer ${token}`, };
+    } else {
+        return { 'Content-Type': 'application/json' };
+    }
+}
