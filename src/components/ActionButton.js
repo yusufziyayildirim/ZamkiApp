4c503b3de4e7f4b24456a5c5481bdec9d0302ede
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-11-28 14:04:37 +0300

    Update ActionButton.js

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -1,15 +1,23 @@
 import { useTheme } from "@react-navigation/native";
 import { Text, TouchableOpacity } from "react-native";
 
+//Store
+import { useDispatch } from "react-redux";
+import { reset } from "../store/auth/authSlice";
+
 const ActionButton = ({ value, onPress = null, disabled = false, height, width }) => {
     const colors = useTheme().colors;
+
+    const dispatch = useDispatch()
+
     const handlePress = () => {
+        dispatch(reset())
         onPress()
     }
     return (
         <TouchableOpacity
             disabled={disabled}
-            style={{ backgroundColor: colors.primary, height: height ?? 55, width: width ?? "100%" , borderRadius: 15, alignItems: "center", justifyContent: "center", marginTop: 10 }}
+            style={{ backgroundColor: colors.primary, height: height ?? 55, width: width ?? "100%", borderRadius: 15, alignItems: "center", justifyContent: "center", marginTop: 10 }}
             onPress={handlePress}
         >
             {disabled
