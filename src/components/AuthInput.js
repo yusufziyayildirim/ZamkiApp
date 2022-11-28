8ad46d7f4b63c71b5cc85d4e0c5d047bc241d6b5
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-11-28 14:03:39 +0300

    Update AuthInput.js

--- src/components/AuthInput.js
+++ src/components/AuthInput.js
@@ -1,31 +1,47 @@
-import { TextInput } from "react-native";
+import { useState } from "react";
+import { TextInput, View } from "react-native";
 import { useTheme } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
 
-const AuthInput = ({ placeholder, value, onChange }) => {
+const AuthInput = ({ placeholder, value, onChange, secure = null }) => {
     const colors = useTheme().colors;
-
+    const [isSecure, setIsSecure] = useState(secure)
     return (
-        <TextInput
-            placeholder={placeholder}
-            placeholderTextColor="#bdbbc2"
-            style={{
-                height: 55,
-                borderRadius: 15,
-                backgroundColor: colors.lightGray,
-                color: colors.textPrimary,
-                paddingHorizontal: 22,
-                shadowColor: "#000",
-                shadowOffset: {
-                    width: 0,
-                    height: 4,
-                },
-                shadowOpacity: 0.1,
-                shadowRadius: 24,
-                marginBottom: 20
-            }}
-            value={value}
-            onChangeText={(value) => onChange(value)}
-        />
+        <View style={{ position: "relative" }}>
+            <TextInput
+                placeholder={placeholder}
+                placeholderTextColor="#bdbbc2"
+                autoCapitalize="none"
+                secureTextEntry={isSecure}
+                value={value}
+                onChangeText={(value) => onChange(value)}
+                style={{
+                    height: 55,
+                    borderRadius: 15,
+                    backgroundColor: colors.lightGray,
+                    color: colors.textPrimary,
+                    paddingHorizontal: 22,
+                    shadowColor: "#000",
+                    shadowOffset: {
+                        width: 0,
+                        height: 4,
+                    },
+                    shadowOpacity: 0.1,
+                    shadowRadius: 24,
+                    marginBottom: 20
+                }}
+            />
+            {
+                isSecure != null && value &&
+                <Ionicons
+                    name={isSecure ? 'eye-off' : 'eye'}
+                    style={{ position: "absolute", right: 10, paddingVertical: 15 }}
+                    size={24}
+                    color={colors.primary}
+                    onPress={() => setIsSecure(!isSecure)}
+                />
+            }
+        </View>
     )
 }
 
