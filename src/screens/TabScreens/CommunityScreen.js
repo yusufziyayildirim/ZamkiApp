f0f78e7c01c11f9d57cc46b407321799df45e4d1
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-12-06 11:15:14 +0300

    CommunityScreen designed

--- src/screens/TabScreens/CommunityScreen.js
+++ src/screens/TabScreens/CommunityScreen.js
@@ -1,13 +1,93 @@
-import { View, TouchableOpacity, Text, Image } from 'react-native';
-import { useTheme } from '@react-navigation/native';
+import { TouchableOpacity } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
+//Components
+import UserListItem from '../../components/UserListItem';
+import SearchBox from '../../components/SearchBox';
+import TabBarScreenWrap from '../../components/TabBarScreenWrap';
+import TabBarPageTitle from '../../components/TabBarPageTitle';
 
 const CommunityScreen = () => {
-  const colors = useTheme().colors;
 
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <TabBarScreenWrap>
+      <TabBarPageTitle title="Community" />
+      <KeyboardAwareScrollView style={{ height: "97.7%" }} keyboardShouldPersistTaps='handled'>
+        <SearchBox />
+        <TouchableOpacity>
+          <UserListItem
+            userImg="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+            userName="Yusuf"
+            userDetail="hiiiiii"
+            speaks=""
+            learns=""
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <UserListItem
+            userImg="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+            userName="Yusuf"
+            userDetail="hiiiiii"
+            speaks=""
+            learns=""
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <UserListItem
+            userImg="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+            userName="Yusuf"
+            userDetail="hiiiiii"
+            speaks=""
+            learns=""
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <UserListItem
+            userImg="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+            userName="Yusuf"
+            userDetail="hiiiiii"
+            speaks=""
+            learns=""
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <UserListItem
+            userImg="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+            userName="Yusuf"
+            userDetail="hiiiiii"
+            speaks=""
+            learns=""
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <UserListItem
+            userImg="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+            userName="Yusuf"
+            userDetail="hiiiiii"
+            speaks=""
+            learns=""
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <UserListItem
+            userImg="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+            userName="Yusuf"
+            userDetail="hiiiiii"
+            speaks=""
+            learns=""
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <UserListItem
+            userImg="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+            userName="Yusuf"
+            userDetail="hiiiiii"
+            speaks=""
+            learns=""
+          />
+        </TouchableOpacity>
+      </KeyboardAwareScrollView>
+    </TabBarScreenWrap>
   )
 }
 
