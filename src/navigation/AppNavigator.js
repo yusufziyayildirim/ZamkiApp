87a154c69d3057e9114db7f2f67e4e216b93ed0f
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-12-09 10:49:43 +0300

    Update AppNavigator.js

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,4 +1,4 @@
-import { View, TouchableOpacity, Text, Image } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useTheme } from '@react-navigation/native';
@@ -19,6 +19,7 @@ import LanguagesScreen from '../screens/AppScreens/LanguagesScreen';
 import EditLanguagesScreen from '../screens/AppScreens/EditLanguagesScreen';
 import ChangePasswordScreen from '../screens/AppScreens/ChangePasswordScreen';
 import UserDetailScreen from '../screens/AppScreens/UserDetailScreen';
+import ReferencesScreen from '../screens/AppScreens/ReferencesScreen';
 
 const Tab = createBottomTabNavigator();
 const AppStack = createNativeStackNavigator();
@@ -177,6 +178,29 @@ function AppNavigator() {
                     headerRight: () => (
                         <TouchableOpacity style={{ padding: 5 }}>
                             <FontAwesome name="exclamation-circle" size={22} color="#fff" />
+                        </TouchableOpacity>
+                    )
+                })}
+            />
+            <AppStack.Screen
+                name="References"
+                component={ReferencesScreen}
+                options={({ navigation, route }) => ({
+                    title: "References",
+                    headerShadowVisible: false,
+                    headerTintColor: colors.textPrimary,
+                    headerTitleAlign: 'center',
+                    headerStyle: {
+                        backgroundColor: colors.background
+                    },
+                    headerLeft: () => (
+                        <TouchableOpacity
+                            style={{ padding: 5 }}
+                            onPress={
+                                () => navigation.goBack()
+                            }
+                        >
+                            <FontAwesome name="chevron-left" size={20} color={colors.textPrimary} />
                         </TouchableOpacity>
                     )
                 })}
