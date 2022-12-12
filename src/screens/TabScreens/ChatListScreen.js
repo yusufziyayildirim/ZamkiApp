9810d88d3faf079cf18f86c776fb8ae365df1df3
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-12-12 15:06:14 +0300

    ChatListScreen designed

--- src/screens/TabScreens/ChatListScreen.js
+++ src/screens/TabScreens/ChatListScreen.js
@@ -1,13 +1,43 @@
-import { View, TouchableOpacity, Text, Image } from 'react-native';
-import { useTheme } from '@react-navigation/native';
+import { useState } from 'react';
+import { View, TouchableOpacity, ScrollView } from 'react-native';
+
+//Components
+import SearchBox from '../../components/SearchBox'
+import ChatListItem from '../../components/ChatListItem';
+import TabBarScreenWrap from '../../components/TabBarScreenWrap';
+import TabBarPageTitle from '../../components/TabBarPageTitle';
 
 const ChatListScreen = () => {
-  const colors = useTheme().colors;
+    const [search, setSearch] = useState("");
 
     return (
-        <View>
-            <Text>ChatList</Text>
-        </View>
+        <TabBarScreenWrap>
+            <TabBarPageTitle title="Messages" />
+            <ScrollView style={{ height: "97.5%" }} keyboardShouldPersistTaps='handled'>
+                <SearchBox search={search} setSearch={setSearch} />
+                <View>
+                    <TouchableOpacity
+                        onPress={() => { }}
+                    >
+                        <ChatListItem
+                            userImg=""
+                            userName={"Yusuf Ziya"}
+                            userMessage={"Deneme mesajı"}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => { }}
+                    >
+                        <ChatListItem
+                            userImg=""
+                            userName={"Yusuf Ziya"}
+                            userMessage={"Deneme mesajı"}
+                            countNewMessage={3}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </TabBarScreenWrap>
     )
 }
 
